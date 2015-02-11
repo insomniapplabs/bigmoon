106d5bf1e0ce2b3513c84f19eecfc89ad2eacdc6
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-02-11 11:06:20 -0600

    change background

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('bigmoonApp', [
   'btford.socket-io',
   'ui.router',
   'ui.bootstrap',
-  'smoothScroll'
+  'smoothScroll',
+  'ngDialog'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
